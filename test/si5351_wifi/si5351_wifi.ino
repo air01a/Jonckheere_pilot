4311af80155eef1759900acd75c04b8024572152
Erwan <erwan@erwan.te>
2024-09-03 21:13:18 +0200

    change frequencies by udp packet

--- test/si5351_wifi/si5351_wifi.ino
+++ test/si5351_wifi/si5351_wifi.ino
@@ -19,35 +19,124 @@ WiFiUDP Udp;
 unsigned int localUdpPort = 4000;  // Port pour recevoir les paquets UDP
 char incomingPacket[255];  // Buffer pour les paquets entrants
 
+
+typedef struct {
+    uint8_t pll_mult;    // a
+    uint32_t pll_num;    // b
+    uint32_t pll_denom;  // c
+    uint32_t ms_divider; // d
+    uint32_t ms_num;     // e
+    uint32_t ms_denom;   // f
+    uint8_t r_div;       // R
+} FrequencyParams;
+
+#define NUM_FREQUENCIES 3
+#define CLOCK_OUTPUT 2
+#define CLOCK_PLL SI5351_PLL_A
+
+si5351RDiv_t r_div = SI5351_R_DIV_64;
+int freq_index = 0;
+
+FrequencyParams frequencies[NUM_FREQUENCIES] = {
+    // Fréquence 1
+    {24, 23607, 997652, 697, 0, 1, SI5351_R_DIV_64}, // "sidereal"
+    // Fréquence 2
+    {24, 6293, 234048, 699, 0, 1, SI5351_R_DIV_64}, // "solar"
+    // Fréquence 3
+    {24, 4303, 526562, 723, 0, 1, SI5351_R_DIV_64}  // "lunar"
+};
+
+typedef void (*CommandFunction)();  // Définir un type pour le pointeur de fonction
+
+typedef struct {
+    const char* command;
+    CommandFunction function;
+} Command;
+
+
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
 
+
+
+void set_frequency() {
+  FrequencyParams params = frequencies[freq_index];
+  clockgen.setupPLL(CLOCK_PLL,params.pll_mult,params.pll_num,params.pll_denom);
+  clockgen.setupMultisynth(CLOCK_OUTPUT,CLOCK_PLL, params.ms_divider, params.ms_num, params.ms_denom);
+  clockgen.setupRdiv(CLOCK_OUTPUT, r_div);
+  clockgen.enableOutputs(true);
+
+}
+
 void setLunar(void)
 {
-  clockgen.setupPLL(SI5351_PLL_A,24,4303,526562);
-  clockgen.setupMultisynth(2, SI5351_PLL_A, 723, 0, 1);
-  clockgen.setupRdiv(2, SI5351_R_DIV_64);
-  clockgen.enableOutputs(true);
+  freq_index=2;
+  set_frequency();
 }
 
 void setSidereal(void)
 {
-  clockgen.setupPLL(SI5351_PLL_A,24,23607,997652);
-  clockgen.setupMultisynth(2, SI5351_PLL_A, 697, 0, 1);
-  clockgen.setupRdiv(2, SI5351_R_DIV_64);
-  clockgen.enableOutputs(true);
+  freq_index=1;
+  set_frequency();
 }
 
 void setSolar(void)
 {
-  clockgen.setupPLL(SI5351_PLL_A,24,6293,234048);
-  clockgen.setupMultisynth(2, SI5351_PLL_A, 699, 0, 1);
-  clockgen.setupRdiv(2, SI5351_R_DIV_64);
-  clockgen.enableOutputs(true);
+  freq_index=0;
+  set_frequency();
 }
+
+void x1(void)
+{
+  r_div = SI5351_R_DIV_64;
+  set_frequency();
+}
+
+void x2(void)
+{
+  r_div = SI5351_R_DIV_32;
+  set_frequency();
+}
+
+void x4(void)
+{
+  r_div = SI5351_R_DIV_16;
+  set_frequency();
+}
+
+void x16(void)
+{
+  r_div = SI5351_R_DIV_4;
+  set_frequency();
+}
+
+#define NUM_COMMANDS 7
+
+Command commands[NUM_COMMANDS] = {
+    {"sidereal", setSidereal},
+    {"lunar", setLunar},
+    {"solar", setSolar},
+    {"x2",x2},
+    {"x1",x1},
+    {"x4",x4},
+    {"x16",x16}
+};
+
+void executeCommand(const char* incomingPacket) {
+  Serial.println(incomingPacket);
+    for (int i = 0; i < NUM_COMMANDS; i++) {
+        if (strcmp(incomingPacket, commands[i].command) == 0) {
+            Serial.println(commands[i].command);
+            commands[i].function();
+            return;
+        }
+    }
+    Serial.println("Chaîne inconnue reçue.");
+}
+
 
 void setup(void)
 {
@@ -90,7 +179,7 @@ void setup(void)
     should go here)
 */
 /**************************************************************************/
-void loop(void)c:\Users\eniquet\dev\Jonckheere_pilot\.git\config
+void loop(void)
 {
   // Vérification des paquets UDP entrants
   int packetSize = Udp.parsePacket();
@@ -101,16 +190,6 @@ void loop(void)c:\Users\eniquet\dev\Jonckheere_pilot\.git\config
       incomingPacket[len] = 0;
     }
     Serial.printf("Reçu %d bytes: %s\n", len, incomingPacket);
-
-    // Vérification du contenu du paquet
-    if (strcmp(incomingPacket, "sidereal") == 0) {
-      Serial.println("Chaîne 'sidereal' reçue. Changement de fréquence...");
-      // Changer la fréquence, par exemple 20 MHz
-      setSidereal();
-    } else if (strcmp(incomingPacket, "lunar") == 0) {
-      setLunar();
-    } else if (strcmp(incomingPacket, "solar") == 0){
-      setSolar(); 
-    }
+    executeCommand(incomingPacket);
   }
 }
